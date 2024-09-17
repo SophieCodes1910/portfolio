8d0074e8b7d174c51d19a6db792855e90eef1565
Sophie <138242849+SophieMcAlavey@users.noreply.github.com>
2024-09-17 22:43:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Initialize EmailJS
-    emailjs.init('gECsT5O4gQUSEfSL6');  // Ensure this is your actual EmailJS User ID
+    emailjs.init('gECsT5O4gQUSEfSL6');
 
     const form = document.getElementById('contact-form');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Prevent form from submitting the default way
+    const submitButton = document.querySelector('.submit-button');
+
+    submitButton.addEventListener('click', () => {
+        // Prevent form from submitting the default way
+        event.preventDefault(); 
 
         // Make sure you're using the correct service and template ID from EmailJS
         emailjs.sendForm('service_gbl8bhj', 'template_6gbewll', form)
@@ -42,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initialize the first slide
     showSlide(currentIndex);
 });
+
 
 
 
